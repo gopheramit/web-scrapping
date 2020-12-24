fe54cb69a9de349a1e104ec40a66526a71291f87
gopheramit <amitagrawal10@outlook.com>
2020-12-24 22:58:17 +0530

    psudo login user

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -36,8 +36,31 @@ func (m *UserModel) Insert(email, password string) error {
 	return nil
 }
 
-func (m *UserModel) Authenticate(email, passwords string) (int, error) {
-	return 0, nil
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ? AND active = TRUE"
+	row := m.DB.QueryRow(stmt, email)
+	err := row.Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	// Check whether the hashed password and plain-text password provided match.
+	// If they don't, we return the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, models.ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	// Otherwise, the password is correct. Return the user ID.
+	return id, nil
 }
 
 func (m *UserModel) Get(id int) (*models.User, error) {
