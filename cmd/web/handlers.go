ba31926ff34619537b5c70f043d7f6fbb8a7a697
gopheramit <amitagrawal10@outlook.com>
2020-12-25 12:00:20 +0530

    logout psudo user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -237,7 +237,15 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+
+	// Remove the authenticatedUserID from the session data so that the user is
+	// 'logged out'.
+	app.session.Remove(r, "authenticatedUserID")
+	// Add a flash message to the session to confirm to the user that they've been
+	// logged out.
+	app.session.Put(r, "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
