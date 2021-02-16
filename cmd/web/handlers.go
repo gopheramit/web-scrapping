b8e480a0d6d87bae5252d20d683c279aa0c08d70
gopheramit <amitagrawal10@outlook.com>
2021-02-16 13:05:46 +0530

    merging with html response

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -242,19 +242,23 @@ func (app *application) linkScrape(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
-	fmt.Println("s:::::::::::", s)
+	if s.Count > 0 {
+		doc, err := goquery.NewDocument("http://jonathanmh.com")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	doc, err := goquery.NewDocument("http://jonathanmh.com")
-	if err != nil {
-		log.Fatal(err)
+		// use CSS selector found with the browser inspector
+		// for each, use index and item
+		doc.Find("body a").Each(func(index int, item *goquery.Selection) {
+			linkTag := item
+			link, _ := linkTag.Attr("href")
+			linkText := linkTag.Text()
+			fmt.Printf("Link #%d: '%s' - '%s'\n", index, linkText, link)
+		})
+		s.Count = s.Count - 1
+
 	}
+	//fmt.Println("s:::::::::::", s)
 
-	// use CSS selector found with the browser inspector
-	// for each, use index and item
-	doc.Find("body a").Each(func(index int, item *goquery.Selection) {
-		linkTag := item
-		link, _ := linkTag.Attr("href")
-		linkText := linkTag.Text()
-		fmt.Printf("Link #%d: '%s' - '%s'\n", index, linkText, link)
-	})
 }
